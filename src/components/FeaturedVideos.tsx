ebe805be49d6985ee99eaf0c4cc8b903e47a80ac
Layo-Dev <benedictchinaza765@gmail.com>
2026-05-21 04:52:43 +0100

    Update FeaturedVideos.tsx

--- src/components/FeaturedVideos.tsx
+++ src/components/FeaturedVideos.tsx
@@ -183,7 +183,7 @@ const FeaturedVideos = () => {
       {/* Sponsored ad */}
       {/*<div className=" mb-4 mx-auto flex h-[160px] w-full max-w-sm items-center justify-center rounded-md border border-dashed border-border text-xs uppercase tracking-widest text-muted-foreground">*/}
         {/*Sponsored · Ad 300×250*/}
-        {/*<BannerAd zoneId="5929334" />*/}
+        <BannerAd zoneId="5929334" />
       {/*</div>*/}
       <h2 className="text-center text-4xl sm:text-6xl font-bold text-white tracking-tight">
         FEATURED VIDEOS
