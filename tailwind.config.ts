42e8e67a2c809d6285041164ce3d6d52a6693979
Layo-Dev <benedictchinaza765@gmail.com>
2026-06-08 15:00:16 +0000

    Add primary2 color definition to Tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -32,6 +32,7 @@ export default {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))",
         },
+        primary2: "hsl(var(--primary2))",
         destructive: {
           DEFAULT: "hsl(var(--destructive))",
           foreground: "hsl(var(--destructive-foreground))",
